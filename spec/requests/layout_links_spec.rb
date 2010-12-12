f956ed57b9974f7d696d4f98a43a91f0e926ea88
rforte <rforte@gmail.com>
2010-12-12 14:49:50 -0500

    layout stuff

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,5 +25,21 @@ describe "LayoutLinks" do
   it "should have a signup page at '/signup'" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
-  end  
+  end
+  
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should # fill in
+    click_link "Contact"
+    response.should # fill in
+    click_link "Home"
+    response.should # fill in
+    click_link "Sign up now!"
+    response.should # fill in
+  end
+      
 end
